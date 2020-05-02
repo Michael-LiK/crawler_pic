29d74e0274aaf4b819586aead8732b2cbbc35ecd
小michael <liyuhao@bilibili.com>
2020-05-02 17:36:23 +0800

    dev

---
+++ config.py
@@ -0,0 +1,5 @@
+proxyinfo = "http://%s:%s@%s:%s" % ('H041M2M26HDB5E7D', 'AFC83D036396CE05', 'http-dyn.abuyun.com', '9020')
+proxy_config = {
+    "proxy" : {"http": proxyinfo, "https": proxyinfo},
+    "open" : "false"
+}
---
+++ crawler.py
@@ -0,0 +1,69 @@
+import requests
+import re
+import os
+import multiprocessing
+import config
+
+
+class meitulu:
+
+    def get_pic_list(self, url):
+        response = requests.get(url)
+        response.encoding = 'utf-8'
+        pic_num_search = re.compile(r'图片： (\d+) 张')
+        pic_index_search = re.compile(r'href="https://www.meitulu.com/item/(\d+).html"')
+        pic_tag_search = re.compile(r'class="tags">(\w+)</a>')
+        pic_title_search = re.compile(r'target="_blank">(.*?)</a>')
+        pic_num = pic_num_search.findall(response.text)
+        pic_index = pic_index_search.findall(response.text)
+        pic_tag = pic_tag_search.findall(response.text)
+        pic_title = pic_title_search.findall(response.text)
+        pic_index = pic_index[0::2]
+        pic_title = pic_title[1::2]
+
+        i = 0
+        res = []
+        for value in pic_num:
+            pic_info = {}
+            pic_info['index'] = pic_index[i]
+            pic_info['title'] = pic_title[i]
+            pic_info['tag'] = pic_tag[i]
+            pic_info['num'] = value
+            i = i + 1
+            res.append(pic_info)
+        return res
+
+    def download_img(self, info, proxy_config):
+
+        print('开始下载' + info['title'])
+        dirname = 'd:\\crawler\\' + info['title']
+
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+
+        for i in range(1, int(info['num']) + 1):
+            img_url = 'https://mtl.gzhuibei.com/images/img/' + info['index'] + '/' + str(i) + '.jpg'
+            print(img_url)
+            if (config.proxy_config['open'] == "true"):
+                response = requests.get(url=img_url, proxies=proxy_config['proxy'])
+            else:
+                response = requests.get(url=img_url)
+            if response.status_code == 200:
+                filename = dirname + '/' + str(i) + '.jpg'
+                # 打开文件夹并写入图片
+                with open(filename, 'wb') as f:
+                    f.write(response.content)
+        print('下载完成' + info['title'])
+
+
+if __name__ == '__main__':
+    client = meitulu()
+    pic_list = client.get_pic_list("https://www.meitulu.com/t/minidamengmeng/")
+    # 配置进程池
+    pool = multiprocessing.Pool(5)
+
+    for value in pic_list:
+        pool.apply_async(client.download_img, args=(value, config.proxy_config))
+    pool.close()
+    pool.join()
+    print("爬虫执行结束")
